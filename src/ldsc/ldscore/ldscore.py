d709dd94a37b7928a016a7c5d53f2534d96a3621
Ashwini Kumar Sharma <46895034+ashwini-kr-sharma@users.noreply.github.com>
2024-11-27 12:05:21 +0000

    Resolve concatenation error between Path and String in ldscore.py
    In Lines 782-784 and 787-789 in ldscore.py the code tries to concatenate a Path object with a regular String using the + operator which throws an error

--- src/ldsc/ldscore/ldscore.py
+++ src/ldsc/ldscore/ldscore.py
@@ -781,14 +781,12 @@ def ldscore(args):
         M_5_50 = [np.sum(geno_array.maf > 0.05)]
 
     # print .M
-    fout_M = open(args.out + "." + file_suffix + ".M", "wb")
-    print("\t".join(map(str, M)), file=fout_M)
-    fout_M.close()
+    with open(f"{args.out}.{file_suffix}.M", "wb") as fout_M:
+        fout_M.write("\t".join(map(str, M)).encode() + b'\n')
 
     # print .M_5_50
-    fout_M_5_50 = open(args.out + "." + file_suffix + ".M_5_50", "wb")
-    print("\t".join(map(str, M_5_50)), file=fout_M_5_50)
-    fout_M_5_50.close()
+    with open(f"{args.out}.{file_suffix}.M_5_50", "wb") as fout_M_5_50:
+        fout_M_5_50.write("\t".join(map(str, M_5_50)).encode() + b'\n')
 
     # print annot matrix
     if (args.cts_bin is not None) and not args.no_print_annot:
