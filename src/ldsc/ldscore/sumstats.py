3458cab063a13fdbdb568737b04f0accf59e6efc
Ashwini Kumar Sharma <46895034+ashwini-kr-sharma@users.noreply.github.com>
2024-11-27 13:32:41 +0000

    Fix path issue in sumstats.py
    args.out is a Path object and does not support concatenation to a string using the "+" operator

--- src/ldsc/ldscore/sumstats.py
+++ src/ldsc/ldscore/sumstats.py
@@ -379,7 +379,7 @@ def cell_type_specific(args):
 
     df_results = pd.DataFrame(data=results_data, columns=results_columns)
     df_results.sort_values(by="Coefficient_P_value", inplace=True)
-    df_results.to_csv(args.out + ".cell_type_results.txt", sep="\t", index=False)
+    df_results.to_csv(str(args.out) + ".cell_type_results.txt", sep="\t", index=False)
     logger.info(f"Results printed to {args.out}.cell_type_results.tx")
 
 
